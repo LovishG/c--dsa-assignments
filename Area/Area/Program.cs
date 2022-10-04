79fb6868a97f4f6df2748b5249dbc64f7c15884a
LovishG <lovish0320.cse19@chitkara.edu.in>
2022-10-04 16:33:51 +0530

    first commit

--- Area/Area/Program.cs
+++ Area/Area/Program.cs
@@ -14,21 +14,6 @@ public class Program
 
 
 
-    public static double RedArea(int r)
-    {
-        // If circle is centered at the origin, point of intersection found by substitution (-3r/5, -4r/5)
-
-        double theta = Math.Atan2(4, 3) * 2;
-        double areaOfPieSlice = theta * r * r / 2.0;
-        double areaOfTriangleInCircle = 2 * (4 / 5.0) * (3 / 5.0) / 2 * r * r;
-        double areaOfLeftSliceOfHalfCircle = areaOfPieSlice - areaOfTriangleInCircle;
-        double areaOfHalfCircle = Math.PI * r * r / 2.0;
-        double areaOfRightSlideOfHalfCircle = areaOfHalfCircle - areaOfLeftSliceOfHalfCircle;
-        double areaOfRectangleOverRightSideOfHalfCircle = 3 / 5.0 * 2 * r * r;
-        double areaOfRightRedUnderCircle = (areaOfRectangleOverRightSideOfHalfCircle - areaOfRightSlideOfHalfCircle) / 2.0;
-        double areaOfLeftRedTriangle = (r - 3 * r / 5.0) * (r - 4 * r / 5.0) / 2.0;
-        double areaOfRed = areaOfLeftRedTriangle + areaOfRightRedUnderCircle;
-
-        return Math.Round(areaOfRed, 3);
-    }
+    public static double RedArea(int r) => 
+        Math.Round(Math.Pow(r, 2) * (Math.Atan(1d / 2d) + .4d - Math.PI / 4), 3);
 }
